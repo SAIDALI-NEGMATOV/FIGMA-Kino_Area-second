2a1792d68bf3f170f3fc8961e44dc3bfd60b8348
SAYYIDALI2706 <114974636+SAYYIDALI2706@users.noreply.github.com>
2023-04-13 18:59:55 +0500

    Controll

--- src/App.js
+++ src/App.js
@@ -15,13 +15,28 @@ import Films from "./Films/Films";
 import Actors from "./Actors/Actors";
 import { ItemProduct } from "./Components/ItemProduct";
 import { Components } from "./Components/Components";
+import img1 from './Components/images/Jokker.jpg'
+import img2 from './Components/images/Escape.jpg'
+import img3 from './Components/images/opoqi.jpg'
+import img4 from './Components/images/Xennam.jpg'
+import img5 from './Components/images/Ford.jpg'
+import img6 from './Components/images/3022.jpg'
+import img7 from './Components/images/Xarli.jpg'
+import img8 from './Components/images/Police.jpg'
 
-function DataFilms(id, name) {
-  return { id, name };
+function DataFilms(id, name, defImg,Name,Xeshtag) {
+  return { id, name , defImg,Name,Xeshtag};
 }
 
 let films = [
-  DataFilms(1, "Jokker1"),
+  DataFilms(1, "Escape", img2,'Побег из Претории','Триллер'),
+  DataFilms(2, "Joker", img1,'Джокер','Триллер, драма, криминал'),
+  DataFilms(3, "Star", img3,'Звёздные войны: Скайуокер. Восход','Фантастика, фэнтези, боевик, приключения'),
+  DataFilms(4, "Xenam", img4,'Джентльмены','Боевик, комедия, криминал',),
+  DataFilms(5, "Ford", img5,'Ford против Ferrari','Биография, спорт, драма, боевик',),
+  DataFilms(6, "3022", img6,'3022','Фантастика, триллер',),
+  DataFilms(7, "Харли", img7,' Потрясающая история Харли Квинн','Боевик, криминал, комедия',),
+  DataFilms(8, "парни", img8,' Плохие парни навсегда','Боевик, комедия, криминал',),
   // DataFilms(2,"Jokker2"),
   // DataFilms(3,"Jokker3"),
   // DataFilms(4,"Jokker4"),
@@ -37,7 +52,7 @@ export default function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Components films={films} />} />
-          <Route path="/components/:id" element={<ItemProduct films={films} />} />
+          <Route path="/components/:filmId" element={<ItemProduct films={films} />} />
           <Route path="/afisha" element={<Afisha />} />
           <Route path="/media" element={<Media />} />
           <Route path="/news" element={<News />} />
--- src/Components/ItemProduct.js
+++ src/Components/ItemProduct.js
@@ -1,8 +1,11 @@
 import { Box,Typography } from '@mui/material'
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const ItemProduct = ({films}) => {
+  let {filmId}=useParams()
+  console.log(filmId);
   return (
-    <Box><Typography variant='h1'>{films[0].name}</Typography></Box>
+    <Box><Typography variant='h1'>{films[filmId-1].name}</Typography></Box>
   )
 }
