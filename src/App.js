04f52b54f35c84f14b9c6620b07bb0ab6f523fa2
SAYYIDALI2706 <114974636+SAYYIDALI2706@users.noreply.github.com>
2023-04-14 17:24:48 +0500

    ItemProduct

--- src/App.js
+++ src/App.js
@@ -15,34 +15,97 @@ import Films from "./Films/Films";
 import Actors from "./Actors/Actors";
 import { ItemProduct } from "./Components/ItemProduct";
 import { Components } from "./Components/Components";
-import img1 from './Components/images/Jokker.jpg'
-import img2 from './Components/images/Escape.jpg'
-import img3 from './Components/images/opoqi.jpg'
-import img4 from './Components/images/Xennam.jpg'
-import img5 from './Components/images/Ford.jpg'
-import img6 from './Components/images/3022.jpg'
-import img7 from './Components/images/Xarli.jpg'
-import img8 from './Components/images/Police.jpg'
+import img1 from "./Components/images/Jokker.jpg";
+import img2 from "./Components/images/Escape.jpg";
+import img3 from "./Components/images/opoqi.jpg";
+import img4 from "./Components/images/Xennam.jpg";
+import img5 from "./Components/images/Ford.jpg";
+import img6 from "./Components/images/3022.jpg";
+import img7 from "./Components/images/Xarli.jpg";
+import img8 from "./Components/images/Police.jpg";
+import bgIMG from "./Components/images/BGA1.jpg";
+import Static from "./Components/images/Statistic.png";
+import Static2 from "./Components/images/IMDb.png";
 
-function DataFilms(id, name, defImg,Name,Xeshtag) {
-  return { id, name , defImg,Name,Xeshtag};
+function DataFilms(
+  id,
+  name,
+  defImg,
+  Name,
+  Xeshtag,
+  bgimg,
+  Fon,
+  Menyu,
+  Text,
+  Info,
+  Static,
+  Static2,
+  Text2
+) {
+  return {
+    id,
+    name,
+    defImg,
+    Name,
+    Xeshtag,
+    bgimg,
+    Fon,
+    Menyu,
+    Text,
+    Info,
+    Static,
+    Static2,
+    Text2,
+  };
 }
 
 let films = [
-  DataFilms(1, "Escape", img2,'Побег из Претории','Триллер'),
-  DataFilms(2, "Joker", img1,'Джокер','Триллер, драма, криминал'),
-  DataFilms(3, "Star", img3,'Звёздные войны: Скайуокер. Восход','Фантастика, фэнтези, боевик, приключения'),
-  DataFilms(4, "Xenam", img4,'Джентльмены','Боевик, комедия, криминал',),
-  DataFilms(5, "Ford", img5,'Ford против Ferrari','Биография, спорт, драма, боевик',),
-  DataFilms(6, "3022", img6,'3022','Фантастика, триллер',),
-  DataFilms(7, "Харли", img7,' Потрясающая история Харли Квинн','Боевик, криминал, комедия',),
-  DataFilms(8, "парни", img8,' Плохие парни навсегда','Боевик, комедия, криминал',),
-  // DataFilms(2,"Jokker2"),
-  // DataFilms(3,"Jokker3"),
-  // DataFilms(4,"Jokker4"),
-  // DataFilms(5,"Jokker5"),
-  // DataFilms(6,"Jokker6"),
-  // DataFilms(7,"Jokker7"),
+  DataFilms(
+    1,
+    "Escape",
+    img2,
+    "Побег из Претории",
+    "Триллер",
+    bgIMG,
+    img2,
+    "Главная > Фильмы > Побег из Претории",
+    "Побег из Претории",
+    "Escape from Pretoria",
+    Static,
+    Static2,
+    "Двое борцов за свободу отбывают срок в одной из самых строгих тюрем мира — в «Претории». Вместе с другими узниками они планируют дерзкий и опасный побег. Но придумать план — это только первый шаг. Шаг второй — реализация плана."
+  ),
+  DataFilms(2, "Joker", img1, "Джокер", "Триллер, драма, криминал"),
+  DataFilms(
+    3,
+    "Star",
+    img3,
+    "Звёздные войны: Скайуокер. Восход",
+    "Фантастика, фэнтези, боевик, приключения"
+  ),
+  DataFilms(4, "Xenam", img4, "Джентльмены", "Боевик, комедия, криминал"),
+  DataFilms(
+    5,
+    "Ford",
+    img5,
+    "Ford против Ferrari",
+    "Биография, спорт, драма, боевик"
+  ),
+  DataFilms(6, "3022", img6, "3022", "Фантастика, триллер"),
+  DataFilms(
+    7,
+    "Харли",
+    img7,
+    " Потрясающая история Харли Квинн",
+    "Боевик, криминал, комедия"
+  ),
+  DataFilms(
+    8,
+    "парни",
+    img8,
+    " Плохие парни навсегда",
+    "Боевик, комедия, криминал"
+  ),
 ];
 
 export default function App() {
@@ -52,7 +115,10 @@ export default function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Components films={films} />} />
-          <Route path="/components/:filmId" element={<ItemProduct films={films} />} />
+          <Route
+            path="/components/:filmId"
+            element={<ItemProduct films={films} />}
+          />
           <Route path="/afisha" element={<Afisha />} />
           <Route path="/media" element={<Media />} />
           <Route path="/news" element={<News />} />
--- src/Components/ItemProduct.js
+++ src/Components/ItemProduct.js
@@ -1,11 +1,99 @@
-import { Box,Typography } from '@mui/material'
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import Play from "./images/Play3.png";
+import Icon from "./images/Icons.png";
+import Reyting from "./images/Reyting.png";
+import Ocenka from "./images/Ocenka.png";
 
-export const ItemProduct = ({films}) => {
-  let {filmId}=useParams()
-  console.log(filmId);
+// import { useParams } from "react-router-dom";
+
+export const ItemProduct = ({ films }) => {
+  // let { filmId } = useParams();
+  // console.log(filmId);
   return (
-    <Box><Typography variant='h1'>{films[filmId-1].name}</Typography></Box>
-  )
-}
+    <Box>
+      {films.map((item, index) => (
+        <Box
+          key={index}
+          width={"100%"}
+          height={"100vh"}
+          display={"flex"}
+          justifyContent={"flex-start"}
+          alignItems={"center"}
+          flexDirection={"column"}
+          sx={{
+            backgroundImage: `url(${item.bgimg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "Cover",
+            bgcolor: "rgba(27, 33, 49, 20)",
+            backgroundBlendMode: "color-dodge",
+          }}
+        >
+          <Box
+            mt={"4%"}
+            display={"flex"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            width={"70%"}
+            gap={"50px"}
+          >
+            <Box flexDirection={"column"}>
+              <Box overflow={"hidden"} borderRadius={"10px"} mb={"10px"}>
+                <img src={item.Fon} alt="" />
+              </Box>
+            </Box>
+            <Box
+              flexDirection={"column"}
+              flexWrap={"wrap"}
+              width={"540px"}
+              gap={"50px"}
+              mb="30px"
+            >
+              <Typography variant="p">{item.Menyu}</Typography>
+              <Typography
+                variant="h2"
+                sx={{ color: "rgba(255, 255, 255, 1)", mb: "10px" }}
+              >
+                {item.Text}
+              </Typography>
+              <Typography variant="h5" sx={{ color: "rgba(255, 255, 255, 1)" }}>
+                {item.Info}
+              </Typography>
+              <Box display={"flex"} mb={"30px"} mt="30px">
+                <img src={item.Static} alt="" />
+                <img src={item.Static2} alt="" />
+              </Box>
+              <Typography variant="p" sx={{ color: "rgba(255, 255, 255, 1)" }}>
+                {item.Text2}
+              </Typography>
+              <Box display={"flex"} mt="30px">
+                <Button
+                  sx={{
+                    padding: "40px,20px",
+                    border: "1px solid #fff",
+                  }}
+                >
+                  <Box mt={"5px"} mr={"10px"}>
+                    <img src={Play} alt="" />
+                  </Box>
+                  Смотреть трейлер
+                </Button>
+                <Box mt={"14px"} ml="10px">
+                  <img src={Icon} alt="" />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          <Box display={"flex"} gap={"10px"} width={'70%'}>
+            <Box>
+              <img src={Reyting} alt="" />
+            </Box>
+            <Box>
+              <img src={Ocenka} alt="" />
+            </Box>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+};
